3a04a9f905a5d33d3283ac42674a6714ba497198
FernandoHuilca <fernando.huilca@epn.edu.ec>
2024-07-25 11:48:36 -0500

    Nuevo proyecto con configuraciones necesarias para que compile correctamente, ejemplo de login funcional hasta presentar el login del usuario

---
+++ Poli-Eats-EPN/src/BusinessLogic/LoginUsuarioController.java
@@ -0,0 +1,51 @@
+
+package BusinessLogic;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+public class LoginUsuarioController implements Initializable {
+
+    @FXML
+    private Button Regresar;
+    
+    @FXML
+    public void RegresarLoginAdmOrUser(){
+        try {
+                // Obtener el Stage actual
+                Stage currentStage = (Stage) Regresar.getScene().getWindow();
+
+                // Cargar el archivo FXML para Login
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Presentation/LoginAdminOrUser.fxml"));
+                Parent root = loader.load();
+
+                // Cambiar la escena del Stage actual
+                currentStage.setScene(new Scene(root));
+                currentStage.setTitle("Login");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("No se pudo cargar la interfaz de usuario.");
+                alert.showAndWait();
+            }
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
