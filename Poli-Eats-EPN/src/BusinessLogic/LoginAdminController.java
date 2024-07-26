1ad5e21eca795bcfaac0ea2cf30000fbacdf90f3
FernandoHuilca <fernando.huilca@epn.edu.ec>
2024-07-25 19:53:15 -0500

    Login con correo Usuarios

--- Poli-Eats-EPN/src/BusinessLogic/LoginAdminController.java
+++ Poli-Eats-EPN/src/BusinessLogic/LoginAdminController.java
@@ -1,6 +1,8 @@
 
 package BusinessLogic;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,67 +13,92 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 
 public class LoginAdminController implements Initializable {
-
+    private final String archivoAdministradoresContraseñas = "src/Data/Login/NombresContraseniasAdministradores.txt";
     @FXML
     private Button RegresarButton;
     @FXML
     private Button SolicitarButton;
+     @FXML
+    private Button LogIn;
+    @FXML
+    private TextField txtAdministrador;
+    @FXML
+    private PasswordField txtContraseña;
+
     
     @FXML
     public void RegresarLoginAdmOrUser(){
-        try {
-                // Obtener el Stage actual
-                Stage currentStage = (Stage) RegresarButton.getScene().getWindow();
-
-                // Cargar el archivo FXML para Login
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Presentation/LoginAdminOrUser.fxml"));
-                Parent root = loader.load();
-
-                // Cambiar la escena del Stage actual
-                currentStage.setScene(new Scene(root));
-                currentStage.setTitle("Login");
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText(null);
-                alert.setContentText("No se pudo cargar la interfaz de usuario.");
-                alert.showAndWait();
-            }
+        cambiarVentana("/Presentation/LoginAdminOrUser.fxml","Login admin");
     }
     
     @FXML
     public void SolicitarCuentaAdmin(){
-         try {
-                // Obtener el Stage actual
-                Stage currentStage = (Stage) SolicitarButton.getScene().getWindow();
+        cambiarVentana("/Presentation/SolicitarCuentaAdmin.fxml","SolicitarCuenta"); 
+    }
+    @FXML
+    public void LoginAcceso() {
+        String usuario = txtAdministrador.getText();
+        String contraseña = txtContraseña.getText();
+        boolean credencialesValidas = false;
 
-                // Cargar el archivo FXML para Login
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Presentation/SolicitarCuentaAdmin.fxml"));
-                Parent root = loader.load();
+        try (BufferedReader reader = new BufferedReader(new FileReader(archivoAdministradoresContraseñas))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                String archivoUsuario = linea;
+                String archivoContraseña = reader.readLine();
 
-                // Cambiar la escena del Stage actual
-                currentStage.setScene(new Scene(root));
-                currentStage.setTitle("SolicitarCuenta");
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText(null);
-                alert.setContentText("No se pudo cargar la interfaz de usuario.");
-                alert.showAndWait();
+                if (usuario.equals(archivoUsuario) && contraseña.equals(archivoContraseña)) {
+                    credencialesValidas = true;
+                    break;
+                }
             }
+        } catch (IOException e) {
+            mostrarAlerta("Error", "Ocurrió un error al leer el archivo.");
+            e.printStackTrace();
+        }
+
+        if (credencialesValidas) {
+            cambiarVentana("/Presentation/PantallaInicioBarUsuarios.fxml", "Pantalla de Inicio Bar");
+        } else {
+            mostrarAlerta("Error", "Usuario o contraseña incorrectos.");
+        }
     }
     
+    
+    private void cambiarVentana(String rutaFXML, String titulo) {
+        try {
+            // Obtener el Stage actual
+            Stage currentStage = (Stage) LogIn.getScene().getWindow();
+
+            // Cargar el archivo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(rutaFXML));
+            Parent root = loader.load();
+
+            // Cambiar la escena del Stage actual
+            currentStage.setScene(new Scene(root));
+            currentStage.setTitle(titulo);
+
+        } catch (IOException e) {
+            mostrarAlerta("Error", "No se pudo cargar la interfaz de usuario.");
+            e.printStackTrace();
+        }
+    }
+     private void mostrarAlerta(String titulo, String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
 }
