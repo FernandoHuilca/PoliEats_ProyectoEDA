9a1db05cae4a0ddbf31c84f78935a7ce727ea923
Juan Mateo Quisilema <juan.quisilema@epn.edu.ec>
2024-08-07 21:38:35 -0500

    agregar Botones y frame

---
+++ Poli-Eats-EPN/src/BusinessLogic/Controllers/PantallaDelAdministradorController.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package BusinessLogic.Controllers;
+
+import BusinessLogic.AppBarPoliEats; //Solo es para probar, hay que eliminar 
+import BusinessLogic.MetodosFrecuentes;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ * PantallaProductos.fxml
+ *
+ * @author PC
+ */
+public class PantallaDelAdministradorController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }  
+    
+    @FXML
+    private Button botonAnadirPedido;
+    @FXML
+    private Button botonEliminarPedido;
+    @FXML
+    private Button botonDeProductos;
+    
+    AppBarPoliEats pruebaDelBar = new AppBarPoliEats();
+    
+    @FXML
+    public void anadirPedido(){
+        MetodosFrecuentes.cambiarVentana(((Stage) botonAnadirPedido.getScene().getWindow()), "/Presentation/GenerarPedido.fxml", "Pantalla Generar Pedido");
+    }
+    @FXML 
+    public void eliminarPedido(){
+        pruebaDelBar.eliminarPedido();
+        MetodosFrecuentes.mostrarAlerta("Ventana de Comprobacion", "Comprobar que si entro");
+    }
+    @FXML
+    public void dirigirAPantallaDeProductos(){
+        MetodosFrecuentes.cambiarVentana(((Stage) botonDeProductos.getScene().getWindow()), "/Presentation/GenerarPedido.fxml", "Pantalla De Productos");
+    }
+}
